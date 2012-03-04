7c272f40199e25a741d9e3b1a58852e71df2078b
Luke Hudson <lukeletters@gmail.com>
2012-03-04 12:33:21 -0600

    UPDATE _config.php example

--- _config.php
+++ _config.php
@@ -1,10 +1,15 @@
 <?php
 
 /**
- * Example of registering for File notifications.
+ * Example of registering for File notifications:
  *
  * Object::add_extension('File', 'FileListenerDecorator');
- * 
- * FileListenerDecorator::register_listener('Article', 'on_image_update', 'all');
- *
+ * FileListenerDecorator::register_listener('NewFilesPage', 'on_file_notification', 'all');
  */
+
+#
+# To try the example page, uncomment the lines below,
+# and delete the file example/_manifest_exclude
+#
+# Object::add_extension('File', 'FileListenerDecorator');
+#FileListenerDecorator::register_listener('NewFilesPage', 'on_file_notification', 'all');
